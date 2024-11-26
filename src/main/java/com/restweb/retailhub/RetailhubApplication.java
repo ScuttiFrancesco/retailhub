902da76e224b1f6e99f895f7d811bf36a84330dc
frara <frara@LAPTOP-FP777E2Q>
2024-11-26 18:07:42 +0100

    Si parte

---
+++ src/main/java/com/restweb/retailhub/RetailhubApplication.java
@@ -0,0 +1,13 @@
+package com.restweb.retailhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailhubApplication.class, args);
+	}
+	//http://localhost:8080/swagger-ui/index.html ------ http://localhost:8080/v3/api-docs
+}
